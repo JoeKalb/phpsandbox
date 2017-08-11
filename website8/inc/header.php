a62b8effa13551ada12d55258ee2ac3dbad11e12
JoeKalb <kalb.joseph@gmail.com>
2017-08-11 12:56:46 -0700

    mysql added

---
+++ website8/inc/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Blog Posts</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css">
+</head>
+<body>
+<?php include('navbar.php'); ?>
